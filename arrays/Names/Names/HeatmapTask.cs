493976b418e2add973f4078f3bb65fea40a222a6
Kseniya Tarasevich <tarasevichkseniya11@gmail.com>
2022-04-05 19:58:01 +0300

    new module

---
+++ arrays/Names/Names/HeatmapTask.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Names
+{
+    internal static class HeatmapTask
+    {
+        public static HeatmapData GetBirthsPerDateHeatmap(NameData[] names)
+        {
+            var days = new int[31];
+
+            for (int i = 0; i < days.Length; i++)
+            {
+                days[i] = i + 1;
+            }
+
+            var daysString = new string[31];
+
+            for (var j = 0; j < days.Length; j++)
+                daysString[j] = days[j].ToString();
+
+            var month = new int[12];
+            for (int i = 0; i < month.Length; i++)
+            {
+                month[i] = i + 1;
+            }
+
+            var monthsString = new string[12];
+
+            for (var j = 0; j < month.Length; j++)
+                monthsString[j] = month[j].ToString();
+
+            return new HeatmapData(
+                "Пример карты интенсивностей",
+                new double[,] { { 1, 2, 3 }, { 2, 3, 4 }, { 3, 4, 4 }, { 4, 4, 4 } },
+                daysString,
+                monthsString);
+        }
+    }
+}
+
---
+++ arrays/Names/Names/HistogramTask.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Names
+{
+    internal static class HistogramTask
+    {
+        public static HistogramData GetBirthsPerDayHistogram(NameData[] names, string name)
+        {
+            var days = new int[31];
+            for (int i = 0; i < days.Length; i++)
+            {
+                days[i] = i + 1;
+            }
+
+            var birthsCounts = new double[31]; 
+
+            for (int i = 0; i < names.Length; i++)
+            {
+                if (names[i].Name == name && names[i].BirthDate.Day != 1)
+                    birthsCounts[names[i].BirthDate.Day - 1]++;
+            }
+
+            var daysString = new string[31];
+
+            for (var j = 0; j < days.Length; j++)
+                daysString[j] = days[j].ToString();
+
+            return new HistogramData(
+                string.Format("Рождаемость людей с именем '{0}'", name),
+                daysString,
+                birthsCounts);
+        }
+    }
+}
