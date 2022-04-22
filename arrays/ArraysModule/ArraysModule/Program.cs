ea161cc9bf694c7d493cb98c2717ac4c6deefbab
Kseniya Tarasevich <tarasevichkseniya11@gmail.com>
2022-04-22 19:37:02 +0300

    minor changes

--- arrays/ArraysModule/ArraysModule/Program.cs
+++ arrays/ArraysModule/ArraysModule/Program.cs
@@ -2,7 +2,7 @@
 
 /*
 
-Arr2. Даны два неубывающих массива чисел. Сформировать неубывающие массивы, являющиеся объединением, пересечением и разностью этих двух массивов (разность в смысле мультимножеств).
+
 
 Arr3. Перевести число из системы счисления с основанием A в систему с основанием B. Можно считать, что 2 ≤ A, B ≤ 10, а число дано в виде массива цифр.
 
@@ -38,9 +38,62 @@ namespace ArraysModule
                 Console.Write(array[i] + " ");
         }
 
+        // Arr2.
+        // Даны два неубывающих массива чисел.
+        // Сформировать неубывающие массивы, являющиеся объединением, пересечением и разностью этих двух массивов (разность в смысле мультимножеств).
+
+        public static void MakeNewArray(int[] array1, int[] array2)
+        {
+            int[] unification = new int[array1.Length + array2.Length];
+            int[] intersection = new int[Math.Min(array1.Length, array2.Length)];
+            int[] difference = new int[array1.Length];
+
+            for (int i = 0; i < array1.Length; i++)
+            {
+                unification[i] = array1[i];
+            }
+
+            for (int j = 0; j < array2.Length; j++)
+            {
+                for (int k = 0; k < array1.Length; k++)
+                {
+                    if (array2[j] != unification[k]) unification[array1.Length + 1] = array2[j];
+                }
+            }
+
+            for (int i = 0; i < unification.Length; i++)
+                Console.Write(unification[i] + " ");
+
+            for (int j = 0; j < array2.Length; j++)
+            {
+                for (int k = 0; k < array1.Length; k++)
+                {
+                    if (array2[j] == array1[k]) intersection[j] = array2[j];
+                }
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < intersection.Length; i++)
+                Console.Write(intersection[i] + " ");
+
+            for (int j = 0; j < difference.Length; j++)
+            {
+                for (int k = 0; k < array2.Length; k++)
+                {
+                    if (array1[j] != array2[k]) difference[j] = array1[j];
+                }
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < difference.Length; i++)
+                Console.Write(difference[i] + " ");
+        }
         static void Main(string[] args)
         {
             //MakeOffset(new int[] { 11, 12, 13, 14, 15 }, 2);
+            MakeNewArray(new int[] { 1, 2, 3 }, new int[] { 2, 4 });
         }
     }
 }
