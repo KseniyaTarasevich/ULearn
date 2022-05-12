4af3c27c1881b76de9049ba600dcd98e55fc3f55
Kseniya Tarasevich <tarasevichkseniya11@gmail.com>
2022-05-12 18:31:38 +0300

    new exercise

--- collectios/Exercises/Exercises/Program.cs
+++ collectios/Exercises/Exercises/Program.cs
@@ -1,4 +1,5 @@
-﻿
+﻿using System.Collections.Generic;
+
 namespace Exercises
 {
     class Program
@@ -23,14 +24,17 @@ namespace Exercises
          В массиве чисел найти подмассив (непрерывный отрезок исходного массива) с максимальной суммой.
          */
 
-        public static int Col3(int[] array, int length)
+        public static int[] Col3(int[] array, int length)
         {
             int sum = 0;
             int newSum = 0;
 
+            List<int> list = new List<int>();
+
             for (int i = 0; i < array.Length; i++)
             {
                 sum += array[i];
+                list.Add(array[i]);
 
                 while (length != 0)
                 {
@@ -38,15 +42,45 @@ namespace Exercises
                     {
                         sum += array[j];
                         length--;
+                        list.Add(array[j]);
                     }
                 }
 
                 if (sum > newSum)
+                {
                     newSum = sum;
+                    list.Clear();
+                }
 
                 sum = 0;
             }
-            return sum;
+            return list.ToArray();
+        }
+
+        /* 
+         Col5. 
+         Даны два массива. Входит ли первый массив во второй как подпоследовательность? 
+         Подпоследовательность — это последовательность элементов в том же порядке, но часть элементов может отсутствовать.
+        */
+
+        public static bool Col5(int[] array1, int[] array2)
+        {
+            bool isEnter = false;
+
+            for (int i = 0; i < array1.Length; i++)
+            {
+                for (int j = 0; j < array2.Length; j++)
+                {
+                    if (array1[i] == array2[j])
+                        isEnter = true;
+
+                    break;
+                }
+
+                if (isEnter) break;
+            }
+
+            return isEnter;
         }
 
         static void Main(string[] args)
